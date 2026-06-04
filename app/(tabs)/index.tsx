81abaab722b5daac03071c559cd8d5b7018d279b
Altin Morina <altinmoorina@gmail.com>
2026-06-04 16:20:07 +0200

    Updated welcome text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome Altiniii!</ThemedText>
+        <ThemedText type="title">Welcome Altini,Erjoni!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
